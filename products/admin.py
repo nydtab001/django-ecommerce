c0c3d7afc13908bca5b572771d2a013e1896c8fd
nydtab001 <nydtab001@myuct.ac.za>
2023-10-23 21:20:38 +0000

    admin, models modified

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from products.models import Category
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='category_images')
+    image = models.ImageField(upload_to='category_images', blank=True, null=True)
 
     def __str__(self):
         return self.name
